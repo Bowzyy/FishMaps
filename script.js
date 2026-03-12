c763cb16f3b7622031d2ecbc01997c9d5a360843
Bow <bowzyy24@gmail.com>
2026-03-12 23:01:00 +0000

    Basic Start
    Using OSM data gathered with the overpass API to get data about water features near the users location

---
+++ script.js
@@ -0,0 +1,92 @@
+// USING Overpass Api, a read only API that gathers selected parts of Open Street Map data
+
+async function getLocation() {
+    // async - asyncronous function for network requests
+    const RESULT = document.getElementById("result");
+    const MY_LOCATION = document.getElementById("mycoords");
+    RESULT.innerText = "Getting your location...";
+
+    navigator.geolocation.getCurrentPosition(
+        (pos) => {
+            const LAT = pos.coords.latitude;
+            const LON = pos.coords.longitude;
+            MY_LOCATION.innerText = LAT + ", " + LON;
+            checkWater(LAT, LON);
+        },
+
+        (err) => {
+            console.warn("Geolocation failed:", err.message);
+            RESULT.innerText =
+                "Could not get your location. Using default location for testing...";
+            // Default coordinates to test (Bristol City)
+            const LAT = 51.4545;
+            const LON = -2.5879;
+            MY_LOCATION.innerText = LAT + ", " + LON;
+            checkWater(LAT, LON);
+        },
+        { enableHighAccuracy: true, timeout: 10000 },
+    );
+}
+
+async function checkWater(lat, lon) {
+    const RESULT = document.getElementById("result");
+    RESULT.innerText = "Checking nearby water features...";
+
+    // QUERY
+    // Filter for different water data, NODE - a single point, WAY - a line or polygon, RELATION - a group of nodes
+    // [out:json] - return results in json format
+    // around: 800 -> within 800m
+    const QUERY = `[out:json];(way(around:800,${lat},${lon})["natural"="water"];way(around:800,${lat},${lon})["waterway"];way(around:800,${lat},${lon})["natural"="coastline"];relation(around:800,${lat},${lon})["natural"="water"];relation(around:800,${lat},${lon})["waterway"];relation(around:800,${lat},${lon})["natural"="coastline"];);out;`;
+
+    try {
+        // Fetch - sends a http request to the overpass api interpreter endpoint**
+        // POST method is used because it can return longer data (GET has a length limit)
+        // Use QUERY - the query language stated above used to filter the data
+        const RESPONSE = await fetch(
+            "https://overpass-api.de/api/interpreter",
+            {
+                method: "POST",
+                body: QUERY,
+            },
+        );
+
+        // Convert response from JSON text into a JavaScript object
+        const DATA = await RESPONSE.json();
+
+        // If not near water, return
+        if (!DATA.elements || DATA.elements.length === 0) {
+            RESULT.innerText = "Not near water! (800 meters)";
+            return;
+        }
+
+        let RIVERS = 0,
+            LAKES = 0,
+            COASTLINES = 0;
+
+        // WATER_TYPE.tags - contains OSM data describing the features
+        // natural - lakes and ponds
+        // waterway - running water such as rivers
+        // Currently increments counters, will implement a smaller range and speicific water body
+        DATA.elements.forEach((WATER_TYPE) => {
+            // Some data is missing a tag property, so by going tags?.waterway, it first checks if the tags property exists and is not undefined or null
+            // Mega useful because API overpass elements arent guarnteed to have tags
+            if (WATER_TYPE.tags?.waterway) RIVERS++;
+            if (WATER_TYPE.tags?.natural === "water") LAKES++;
+            if (WATER_TYPE.tags?.natural === "coastline") COASTLINES++;
+        });
+
+        // Update UI based on the information
+
+        RESULT.innerHTML = `
+      Nearby water detected:<br>
+      Rivers/streams: ${RIVERS}<br>
+      Lakes/reservoirs: ${LAKES}<br>
+      Coastline: ${COASTLINES}
+    `;
+
+        // If the API or network fails show this error.
+    } catch (err) {
+        console.error(err);
+        RESULT.innerText = "Error querying water data from Overpass API.";
+    }
+}
